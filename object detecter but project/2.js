05384d9cf69d7e7d10c086d62147d4162f001366
Kaavya-123 <81299350+Kaavya-123@users.noreply.github.com>
2021-11-24 14:37:09 +0530

    Add files via upload

---
+++ object detecter but project/2.js
@@ -0,0 +1,50 @@
+status = "";
+objects= [];
+results = [];
+
+function preload()
+{
+    img = loadImage('table.jpg');
+}
+
+function setup()
+{
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetecter = ml5.objectDetecter('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status = Detecting Objects";
+}
+function modelLoaded()
+{
+    console.log("Model loaded");
+    status = true;
+    objectDetecter.detect(img , gotResults);
+}
+function gotResults()
+{
+    if (error) 
+    {
+        console.error(error);
+    }
+    console.log(gotResults);
+    objects = results;
+}
+
+function back() {
+    window.location.href = 'hi!.html';
+}
+
+function draw()
+    {
+        if(status != "")
+        {
+           for(i=0;i<objects.length;i++)
+           {
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i] + "" + percent + "%", objects[i].x + 15,  objects[i].y + 15);
+            noFill();
+            stroke(r,g,b);
+            rect(objects[i].x, objects[i].y , objects[i].width , objects[i].height);    
+               }
+        }
+    }
---
+++ object detecter but project/name.js
@@ -0,0 +1,10 @@
+function one()
+{
+    window.location.href = '1page.html';
+}
+
+function two()
+{
+    window.location.href = '2page.html';
+}
+
